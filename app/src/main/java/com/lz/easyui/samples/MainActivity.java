e24804496d203292b6216f24f13f36a676d39e9c
liuyu <liuyu@mrocker.com>
2016-06-16 18:18:26 +0800

    添加界面动画

--- app/src/main/java/com/lz/easyui/samples/MainActivity.java
+++ app/src/main/java/com/lz/easyui/samples/MainActivity.java
@@ -3,11 +3,28 @@ package com.lz.easyui.samples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.lz.easyui.activity.LibraryBaseActivity;
+
+public class MainActivity extends LibraryBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initHeader() {
+
+    }
+
+    @Override
+    protected void initWidget() {
+
+    }
+
+    @Override
+    protected void setWidgetState() {
+
     }
 }
